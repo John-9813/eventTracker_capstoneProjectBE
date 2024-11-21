00fa7d6ea7c1760fe6e3e9151dc4544ff255c5fd
John-9813 <john.oliver98.br@gmail.com>
2024-11-21 16:15:40 +0100

    Aggiunta di; ErrorsResponseDTO, ConflictException, UnauthorizeException e ExceptionsHandler

---
+++ src/main/java/johnoliveira/eventTracker_capstoneProject/exceptions/ExceptionsHandler.java
@@ -0,0 +1,45 @@
+package johnoliveira.eventTracker_capstoneProject.exceptions;
+
+import johnoliveira.eventTracker_capstoneProject.dto.ErrorsResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+    public ErrorsResponseDTO handleBadrequest(BadRequestException ex) {
+        return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+    public ErrorsResponseDTO handleUnauthorized(UnauthorizedException ex) {
+        return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorsResponseDTO handleNotFound(NotFoundException ex) {
+        return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT) // 409
+    public ErrorsResponseDTO handleConflict(ConflictException ex) {
+        return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+    public ErrorsResponseDTO handleGeneric(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsResponseDTO("Server error! We'll fix it soon!", LocalDateTime.now());
+    }
+}
+
